41bb78351c83b53b251d0de7c7ce9193e331a709
Sri Sistla <sri.sistla@ril.com>
2022-04-24 09:25:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Razorpay _razorpay;
+
+  @override
+  void initState() {
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    super.initState();
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    showSnackbar("Success");
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    showSnackbar("Failed");
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    showSnackbar("handle external wallet called");
+  }
+
+  showSnackbar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message),
+
+    ));
+  }
+
+  @override
+  void dispose() {
+    _razorpay.clear(); // Removes all listeners
+    super.dispose();
+  }
+
+  var options = {
+    'key': 'rzp_test_lRPhncfAaAMpBu',
+    'amount': 100,
+    'name': 'SK Tech Ltd',
+    'description': 'Demo Item',
+    'prefill': {
+      'contact': '8888888888',
+      'email': 'test@razorpay.com',
+    },
+    'image':"https://yt3.ggpht.com/ytc/AKedOLRt1d4p7bPylasq_66BIC8-k3hkyVjJ2JICQITK=s900-c-k-c0x00ffffff-no-rj"
+
+  };
+
+  void _incrementCounter() {
+    _razorpay.open(options);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'Demo',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/razor_pay.dart
@@ -0,0 +1,8 @@
+
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class CustomRajorPay{
+  static final instance=Razorpay();
+}
+
+
